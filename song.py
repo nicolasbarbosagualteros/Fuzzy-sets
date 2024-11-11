6b85f5dcaf6e36e9bb3df89deaa52d8079d0e78b
Niko <niko@Nikos-MacBook-Pro.local>
2024-11-11 17:20:52 -0500

    Avances funciones en Python

--- song.py
+++ song.py
@@ -1,11 +1,12 @@
 
 from pytubefix import YouTube
+
 import os
 
 link = input("Ingrese el link de YouTube de la canción que desea descargar: ")
 link = str(link)
+yt = YouTube(url=link)
 play_list = [link]
-
 downloaded_count = 0
 
 def download_from_yt(url, total):
@@ -13,7 +14,7 @@ def download_from_yt(url, total):
     try:
         yt = YouTube(url)
         video = yt.streams.filter(only_audio=True).first()
-        destination = './musica_fuzzysets'
+        destination = 'songs/'
         out_file = video.download(output_path=destination)
         base, ext = os.path.splitext(out_file)
         new_file = base + '.mp3'
@@ -26,6 +27,10 @@ def download_from_yt(url, total):
     else:
         downloaded_count += 1
         print(yt.title, "Descargado exitosamente")
+
+
+ruta = "/Users/niko/Fuzzy-sets/songs/"+yt.title+".mp3"
+print("Descargado a la ruta",ruta)
 
 for song in play_list:
     download_from_yt(song, len(play_list))
